cc5b66cd9398bb628e88de77d8f04a1ef4212680
Vishal062 <vs9425348@gmail.com>
2022-06-09 15:17:33 +0530

    Change State Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p className='text-uppercase'>Hello</p>
-    </div>
-  );
+import React, {Component} from "react";
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userName: "Vishal",
+    };
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === 'Vishal' ? 'Sinha' : 'Vishal',
+    });
+  }
+
+
+    render = () => (
+      <div className="container">
+          <div className="col-12">
+              <h2 className="bg-primary text-white text-center p2">
+                {this.state.userName}  To Do list
+              </h2>
+              <button className = "btn btn-secondry m-2"
+              onClick={this.changeStateData}>
+                ChangeData
+              </button>
+          </div>
+      </div>
+    );  
 }
-
-export default App;
